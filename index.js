bd4894e5c60ed15efc1b851e091e059b96d044c3
LeandroDNC <leandromarquesdeusefiel@gmail.com>
2024-05-22 14:07:20 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+        // const nomes = document.querySelectorAll('.liheader');
+
+        // nomes.forEach(nome => {
+        //     nome.addEventListener('click', () => {
+        //         nomes.forEach(outroNome => outroNome.classList.remove('marcado'));
+        //         nome.classList.add('marcado');
+        //     });
+        // });
+
+        // document.addEventListener('click', (event) => {
+        //     if (!event.target.classList.contains('liheader')) {
+        //         nomes.forEach(nome => nome.classList.remove('marcado'));
+        //     }
+        // });
