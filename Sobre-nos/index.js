efb42d1c9efab57f683f701eda1838439ad376e6
LeandroDNC <leandromarquesdeusefiel@gmail.com>
2024-05-30 17:51:00 -0300

    Atualização

--- Sobre-nos/index.js
+++ Sobre-nos/index.js
@@ -1,30 +1,38 @@
-function chat1() {
-    var div = document.getElementById("chat1");
-    var computedStyle = window.getComputedStyle(div);
-    var put = document.getElementById("chat");
+function exibirPopup() {
+    var popup = document.getElementById("popup");
     var icon = document.getElementById("iconchat");
 
-    if (computedStyle.borderBottomStyle === "outset") {
-        div.style.border = "7px inset #1e3441";
-        put.style.display = "block";
-        div.style.width = "25%";
-        icon.src = "assets/iconenviar.svg"
-
-
-        div.classList.remove("collapsed");
-        document.addEventListener('click', closeChat1Outside);
-    } 
-    function closeChat1Outside(event) {
-        if (!div.contains(event.target)) {
-            div.style.border = "7px outset #1e3441";
-            div.style.width = "80px";
-            put.style.display = "none";
-            icon.src = "assets/chat.svg"
-            div.classList.add("collapsed");
-            document.removeEventListener('click', closeChat1Outside);
-        }
+    if (popup.style.display === "none") {
+        popup.style.display = "block";
+        icon.style.display = "none";
+    } else {
+        popup.style.display = "none";
+        icon.style.display = "block";
     }
 }
 
+function fechar() {
+    var popup = document.getElementById("popup");
+    var icon = document.getElementById("iconchat");
+
+    popup.style.display = "none";
+    icon.style.display = "block";
+}
+
+function enviarFeedback() {
+    // Coloque aqui o código para enviar o feedback
+    alert("Feedback enviado!"); // Exemplo: um alerta é exibido quando o botão de enviar feedback é clicado
+}
+
+
+function enviarFeedback() {
+    var feedback = document.getElementById("feedbackText").value;
+    // Aqui você pode enviar o feedback para o servidor e, em seguida, enviar por email
+    // Por exemplo, você pode usar XMLHttpRequest ou fetch para enviar o feedback para o servidor
+    // Depois, o servidor pode enviar um email com o feedback
+    // Este exemplo apenas mostra um alerta com o feedback
+    alert("Feedback enviado: " + feedback);
+
+}
 
 
