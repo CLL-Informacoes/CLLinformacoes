c9b61eb2c5c845569742b240921a738eca23566e
LeandroDNC <leandromarquesdeusefiel@gmail.com>
2024-05-31 11:34:04 -0300

    Finalizando art

--- Sobre-nos/index.js
+++ Sobre-nos/index.js
@@ -1,38 +1,28 @@
 function exibirPopup() {
     var popup = document.getElementById("popup");
     var icon = document.getElementById("iconchat");
+    var feedbackText = document.getElementById("feedbackText");
 
     if (popup.style.display === "none") {
         popup.style.display = "block";
         icon.style.display = "none";
-    } else {
+        feedbackText.focus();
+    } else if (popup.style.display === "block") {
         popup.style.display = "none";
         icon.style.display = "block";
+    } else {
+        popup.style.display = "block";
+        icon.style.display = "none";
+        feedbackText.focus();
     }
 }
 
-function fechar() {
-    var popup = document.getElementById("popup");
-    var icon = document.getElementById("iconchat");
-
-    popup.style.display = "none";
-    icon.style.display = "block";
+function openPopup() {
+    document.getElementById("popupOverlay").style.display = "flex";
 }
 
-function enviarFeedback() {
-    // Coloque aqui o código para enviar o feedback
-    alert("Feedback enviado!"); // Exemplo: um alerta é exibido quando o botão de enviar feedback é clicado
+function closePopup() {
+    document.getElementById("popupOverlay").style.display = "none";
 }
 
-
-function enviarFeedback() {
-    var feedback = document.getElementById("feedbackText").value;
-    // Aqui você pode enviar o feedback para o servidor e, em seguida, enviar por email
-    // Por exemplo, você pode usar XMLHttpRequest ou fetch para enviar o feedback para o servidor
-    // Depois, o servidor pode enviar um email com o feedback
-    // Este exemplo apenas mostra um alerta com o feedback
-    alert("Feedback enviado: " + feedback);
-
-}
-
-
+document.getElementById("menuHamburguer").addEventListener("click", openPopup);
