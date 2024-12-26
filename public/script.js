52cacc7f1ee9ba873df8c8d814111dec2bf3d0ef
CLL-Informacoes <cllinformacoes@gmail.com>
2024-12-26 18:35:33 +0000

    Ajustando erros

--- public/script.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('.hamburguer').addEventListener('click', ()=>{
-    document.querySelector('body').classList.toggle('showSidebar')
-})
-document.querySelector('.menu').addEventListener('click', ()=>{
-    document.querySelector('body').classList.toggle('showSidebar')
-})
